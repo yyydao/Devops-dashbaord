bafa4dd8b3f9606377c49079c69c4c937250df28
wangxiaoqin <731554181@qq.com>
2018-11-08 17:27:46 +0800

    配置管理

--- src/pages/Setting/ConfigManager/Edit/index.jsx
+++ src/pages/Setting/ConfigManager/Edit/index.jsx
@@ -3,26 +3,34 @@ import React, { Component } from 'react';
 import { Input, Popconfirm, Button } from 'antd';
 
 export default class Edit extends Component{
+  constructor(){
+    super();
+    this.state = {
+      editing: false,
+      newValue:''
+    }
+  }
 
     render(){
-        const { editing, defaultValue, handleChange, handleConfirm, handleCancel, isEditing } = this.props;
+        const { defaultValue, handleConfirm, panelIndex, name} = this.props;
+        const {editing, newValue} = this.state
         return(
             <span>
                 {
                     editing ? (
                         <span>
-                            <Input className="config-project-input" defaultValue={defaultValue} onChange={handleChange} />
-                            <Popconfirm title="确定修改吗？" okText="确定" cancelText="取消" onConfirm={handleConfirm}>
+                            <Input className="config-project-input" defaultValue={defaultValue} onChange={(e)=>{this.setState({newValue:e.target.value})}} />
+                            <Popconfirm title="确定修改吗？" okText="确定" cancelText="取消" onConfirm={()=>{this.setState({editing: false});handleConfirm(newValue,panelIndex,name)}}>
                                 <a>保存</a>
                             </Popconfirm>
-                            <Popconfirm title="确定取消修改吗？" okText="确定" cancelText="取消" onConfirm={handleCancel}>
+                            <Popconfirm title="确定取消修改吗？" okText="确定" cancelText="取消" onConfirm={()=>{this.setState({editing: false})}}>
                                 <a>取消</a>
                             </Popconfirm>
                         </span>
                     ) : (
                         <span>
                             <span style={{width: 300}}>{defaultValue}</span>
-                            <Button icon="edit" type="primary" onClick={isEditing}></Button>
+                            <Button icon="edit" type="primary" onClick={()=>{this.setState({editing: true})}}></Button>
                         </span>
                     )
                 }
--- src/pages/Setting/ConfigManager/index.jsx
+++ src/pages/Setting/ConfigManager/index.jsx
@@ -1,57 +1,171 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Breadcrumb, Card, message } from 'antd';
+import { connect } from 'react-redux';
+import { Breadcrumb, Row, Col, Switch, message, Tabs, Form,} from 'antd';
 
-import { reqPost } from '@/api/api';
+import { reqPost, reqGet } from '@/api/api';
 import './index.scss';
+import ConfigPanel from '@/pages/Setting/ConfigManager/ConfigPanel';
 
 const BreadcrumbItem = Breadcrumb.Item;
+const TabPane = Tabs.TabPane;
+const FormItem = Form.Item;
 
 class ConfigManager extends Component{
     constructor(){
         super();
         this.state = {
-            configList: []
+          envList: [],
+          envData: [],
+          testEnv: {
+            "id": 88,
+            "projectId": 62,
+            "category": 0,
+            "name": "111",
+            "jenckinJob": "pTest0919",
+            "passwdBuild": 0,
+            "openTesting": 0,
+            "routineTaskName": "pTest0919",
+            "timedTaskName": "pTest0919",
+            "submitTestTaskName": "pTest0919",
+            "scenes": [
+              {
+                "sceneId": 69,
+                "envId": 88,
+                "projectId": 62,
+                "name": "退出登录",
+                "parentId": 1,
+                "childrens": [
+                  {
+                    "sceneId": 115,
+                    "envId": 88,
+                    "projectId": 62,
+                    "name": "zcq测试场景02",
+                    "parentId": 69,
+                    "childrens": []
+                  },
+                  {
+                    "sceneId": 105,
+                    "envId": 88,
+                    "projectId": 62,
+                    "name": "zcq测试场景03",
+                    "parentId": 69,
+                    "childrens": [{
+                      "sceneId": 205,
+                      "envId": 88,
+                      "projectId": 62,
+                      "name": "zcq测试场景04",
+                      "parentId": 105,
+                      "childrens": []
+                    }]
+                  }
+                ]
+              }
+            ],
+            "checkedScenes":[69, 115, 205, 105]
+          }
         }
     }
 
     componentWillMount(){
-        this.getConfigList();
+        this.getEnvList();
     }
 
-    getConfigList(){
-        reqPost('/config/list').then(res => {
+    getEnvList = () => {
+      const { projectId } = this.props;
+      console.log(this.props)
+      reqGet('/env/list',{projectId}).then(res => {
             if(parseInt(res.code, 0) === 0){
-                this.setState({ configList: res.data });
+                this.setState({ envList: res.data });
+                res.data.map(item=>{
+                  this.getEnvDetail(item.id)
+                })
             }else{
                 message.error(res.msg);
             }
         })
     }
-
+    getEnvDetail = (envId) => {
+      reqGet('/env/envDetails',{
+        envId:envId
+      }).then(res => {
+        if(parseInt(res.code, 0) === 0){
+           let envData =this.state.envData
+          res.data.envId = envId
+          envData.push(res.data)
+          this.setState({envData});
+        }else{
+          message.error(res.msg);
+        }
+      })
+    }
+  onCheck = (checkedScenes,index) => {
+    let envData=this.state.envData
+    envData[index].checkedScenes=checkedScenes
+    this.setState({envData})
+  }
+  onSelect = (selectedKeys, info, index) => {
+    console.log('onSelect',selectedKeys, info, index);
+  }
+  changeEdit = (value,index,key) =>{
+    let envData=this.state.envData
+    envData[index][key]=value
+    this.setState({envData})
+  }
+  changeSwitch = (value,index,type) =>{
+    let envData=this.state.envData
+    envData[index][type]=value?1:0
+    this.setState({envData})
+  }
+  onButtonClick = (index) => {
+    let envData = JSON.parse(JSON.stringify(this.state.envData[index]))
+    let scenes = envData.checkedScenes.map(item=>{
+      let obj = {}
+      obj.envId = envData.envId
+      obj.sceneId = item
+      return obj
+    })
+    envData.scenes = scenes
+    reqPost('/env/updateEnv',envData).then(res => {
+      if(parseInt(res.code, 0) === 0){
+        message.success(res.msg);
+      }else{
+        message.error(res.msg);
+      }
+    })
+  }
     render(){
-        const { configList } = this.state;
-
+        const { envData} = this.state;
         return(
             <div>
                 <Breadcrumb className="devops-breadcrumb">
                     <BreadcrumbItem><Link to="/home">首页</Link></BreadcrumbItem>
                     <BreadcrumbItem>配置管理</BreadcrumbItem>
                 </Breadcrumb>
-                <div className="config-card-list clear">
-                    {
-                        configList.map((item, index) => {
-                            return(
-                                <Link to={`${item.url}/${item.id}`} key={index}>
-                                    <Card hoverable className="config-card">{item.name}</Card>
-                                </Link>
-                            )
-                        })
-                    }
-                </div>
+              <Tabs type="card">
+                {envData&&
+                    envData.map((item,index) =>
+                      <TabPane tab={item.name} key={index} style={{padding:16}}>
+                        <ConfigPanel
+                            envData={item}
+                            panelIndex={index}
+                            treeCheck={this.onCheck}
+                            treeSelect={this.onSelect}
+                            changeEdit={this.changeEdit}
+                            changeSwitch={this.changeSwitch}
+                            onButtonClick={this.onButtonClick}
+                        >
+                        </ConfigPanel>
+                      </TabPane>
+                    )
+                }
+              </Tabs>
             </div>
         )
     }
 }
-
-export default ConfigManager;
+export default connect(state => {
+  return {
+    projectId: state.projectId
+  }
+}, {})(ConfigManager);
