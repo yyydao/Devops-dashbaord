693af619c76007ec4d3ca3dad928db9e5dcac2bf
yyy <yyydao11@gmail.com>
2018-11-30 16:10:46 +0800

    流水线包详情

--- src/pages/Package/pipelinePackageDetail.jsx
+++ src/pages/Package/pipelinePackageDetail.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { reqPost, reqGet } from '@/api/api'
+import { reqGet } from '@/api/api'
 import './list.scss';
 
 import {
     Button,
     Skeleton,
     Modal,
-    message,
     Icon,
     Row,
     Col,
@@ -15,45 +14,8 @@ import {
 } from 'antd'
 
 const {Meta} = Card
-const info = Modal.confirm
-
 const QRCode = require('qrcode.react')
 
-const fakeData = {
-    data: [
-        {
-            'packageType': 1,
-            'fileName': 'app-normal-debug.apk',
-            'versionName': 'V5.4.1',
-            'appFileSize': '26.84MB',
-            'filePath': './package/d53d33cb21e24ce58b1658816491f8852kuV00/Android/code_source/20181124180443/app-normal-debug.apk',
-            'updateTime': '2018-11-24 18:04:43.0',
-
-            'fileType': 1,
-            'recordNo': '',
-            'taskID': ''
-        },
-        {
-            'packageType': 1,
-            'fileName': 'app-normal-debug.apk',
-            'versionName': 'V5.4.1',
-            'appFileSize': '26.84MB',
-            'filePath': './package/d53d33cb21e24ce58b1658816491f8852kuV00/Android/code_source/20181124180443/app-normal-debug.apk',
-
-            'fileType': 1,
-            'recordNo': '',
-            'taskID': ''
-        }
-    ],
-    record: {
-        'buildor': '吴俊',
-        'branchName': 'origin/develop_td',
-        'buildTime': '2018-11-24 18:04:43.0',
-        'buildType': 1,
-        'recordNo': '',
-        'taskID': ''
-    }
-}
 
 class pipelinePackageDetail extends Component {
     constructor (props) {
@@ -98,7 +60,7 @@ class pipelinePackageDetail extends Component {
             content:
                 <div>
                     <Row type="flex" align='middle' justify='center'>
-                        <QRCode value={appUrl} width={250}/>
+                        <QRCode value={appUrl} />
                     </Row>
                     <Row type="flex" align='middle' justify='center' style={{'paddingTop':'16px'}}>
                         <Button><a href={appUrl} target="_blank">点击下载</a></Button>
@@ -109,9 +71,19 @@ class pipelinePackageDetail extends Component {
         })
     }
 
-    jumpToPipelineDetail = (recordNo, taskID) => {
-        this.props.location.replace({
-            pathname: `/pipeline/`
+    jumpToPipelineDetail = (recordNo, taskID,buildId,fileType) => {
+        const inheritFromParent={
+            taskID: 'd53d33cb21e24ce58b1658816491f8852kuV00',
+            recordNo: '954f602a265048a181e164ce79a8e6f2oW3505',
+            buildId:55,
+            fileType:1,
+        }
+        this.props.history.replace({
+            pathname: `/pipeline/packageDetail/${inheritFromParent.taskID}`,
+            search: `?buildNumber=${inheritFromParent.buildId}&curRecordNo=${inheritFromParent.recordNo}&platform=${inheritFromParent.fileType}`,
+            state:{
+                taskStatus:2
+            }
         })
     }
 
@@ -240,7 +212,7 @@ class pipelinePackageDetail extends Component {
                                             </Col>
                                         </Row>
                                     })}
-                                    <Row type="flex" justify="space-around" align="middle" className='detail-row build-info'>
+                                    <Row type="flex" justify="space-around" align="bottom" className='detail-row build-info'>
                                         <Col  span={12}>
                                             <p>触发人：{buildor}</p>
                                             <p>触发类型：{enumBuildType[buildType]}</p>
@@ -249,6 +221,7 @@ class pipelinePackageDetail extends Component {
                                         </Col>
                                         <Col  span={12}>
                                             <Button
+                                                style={{marginBottom:'12px'}}
                                                 onClick={() => this.jumpToPipelineDetail(taskID, recordNo)}>查看报告</Button>
                                         </Col>
                                     </Row>
