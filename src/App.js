ece80f70f839f7a391c9c5a520aa890ccd7f0a27
yyy <yyydao11@gmail.com>
2018-12-07 14:39:15 +0800

    editorconfig

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Route from './router';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Route from './router'
 
-import { setProjectId, setUserInfo } from '@/store/action';
+import { setProjectId, setUserInfo } from '@/store/action'
 
 class App extends Component {
-    componentWillMount(){
-        const projectId = localStorage.getItem('projectId');
-        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-        this.props.setProjectId(projectId);
-        this.props.setUserInfo(userInfo);
-    }
+  componentWillMount () {
+    const projectId = localStorage.getItem('projectId')
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+    this.props.setProjectId(projectId)
+    this.props.setUserInfo(userInfo)
+  }
 
-    render() {
-        return (
-            <Route/>
-        );
-    }
+  render () {
+    return (
+      <Route/>
+    )
+  }
 }
 
 export default connect(state => {
-    return{}
-}, { setProjectId, setUserInfo })(App);
+  return {}
+}, { setProjectId, setUserInfo })(App)
