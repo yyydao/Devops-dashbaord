011b1b03e0ce27ec44e8e70e0418aec39bb0626e
wangxiaoqin <731554181@qq.com>
2018-12-21 18:02:06 +0800

    【BUG修改】仪表盘刻度以及性能测试文字溢出

--- src/pages/Dashboard/chart/PipelineChart1.jsx
+++ src/pages/Dashboard/chart/PipelineChart1.jsx
@@ -73,29 +73,6 @@ class PipelineChart1 extends Component {
     });
     myChart.hideLoading()
   }
-  formatSeconds =(value)=> {
-    var theTime = parseInt(value,10);// 秒
-    var theTime1 = 0;// 分
-    var theTime2 = 0;// 小时
-// alert(theTime);
-    if(theTime > 60) {
-      theTime1 = parseInt(theTime/60,10);
-      theTime = parseInt(theTime%60,10);
-// alert(theTime1+"-"+theTime);
-      if(theTime1 > 60) {
-        theTime2 = parseInt(theTime1/60,10);
-        theTime1 = parseInt(theTime1%60,10);
-      }
-    }
-    var result = ""+parseInt(theTime,10)+"s";
-    if(theTime1 > 0) {
-      result = ""+parseInt(theTime1,10)+"''"+result;
-    }
-    if(theTime2 > 0) {
-      result = ""+parseInt(theTime2,10)+"'"+result;
-    }
-    return result;
-  }
   setOptions=(data)=> {
     const type=['开始','构建','测试','部署','完成','无']
     let legend=[]
