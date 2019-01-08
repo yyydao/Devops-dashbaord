42e827f044ddd44df841ad098f791310dc95e869
wangxiaoqin <731554181@qq.com>
2019-01-08 16:16:41 +0800

    包管理跳转projectId

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Route from './router'
+import qs from 'qs'
 
 import { setProjectId, setUserInfo } from '@/store/action'
 
 class App extends Component {
   componentWillMount () {
-    const projectId = localStorage.getItem('projectId')
+    let projectId = localStorage.getItem('projectId')
+    const parsedHash = qs.parse(window.location.search.slice(1))
+    if(parsedHash.project){
+      projectId=parsedHash.project
+      localStorage.setItem('projectId',projectId)
+    }
     const userInfo = JSON.parse(localStorage.getItem('userInfo'))
     this.props.setProjectId(projectId)
     this.props.setUserInfo(userInfo)
