564a0e3463d4d6fb6661de4a06da712575fae01f
wangxiaoqin <731554181@qq.com>
2019-01-08 16:52:27 +0800

    包管理跳转projectId

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Route from './router'
 import qs from 'qs'
-import getQueryString from './utils'
+import getQueryString from 'utils/utils'
 import { setProjectId, setUserInfo } from '@/store/action'
 
 class App extends Component {
