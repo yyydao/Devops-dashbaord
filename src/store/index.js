5b726479d6c53288eba402b044d897dd14f3e7c0
wangxiaoqin <731554181@qq.com>
2019-02-20 15:00:58 +0800

    代码整理去除redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { logger } from 'redux-logger'
 import promiseMiddleware from 'redux-promise-middleware'
 import reducers from './reducers'
 
@@ -8,7 +7,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 export default function configureStore (preloadedState) {
   const middlewares = [promiseMiddleware({ promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'ERROR'] }), thunkMiddleware]
-  const middlewareEnhancer = applyMiddleware(...middlewares, logger)
+  const middlewareEnhancer = applyMiddleware(...middlewares)
   const enhancers = [middlewareEnhancer]
   const composedEnhancers = composeWithDevTools(...enhancers)
 
